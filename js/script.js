52f8f15c0ae61f9df6d930323e1602bb3b37230d
Sofgren89 <112484097+Sofgren89@users.noreply.github.com>
2023-01-04 20:49:44 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
-
 $("#btn").click(function (event) {
     event.preventDefault();
-    $('.lds-ring').css('visibility','initial')
+    $('.lds-ring').css('visibility', 'initial')
     $('#img-container').html('');
-   
+
     let text = $('#textInput').val();
     text.toLowerCase();
-   
+
+
     if (text == '') {
-        $('.lds-ring').css('visibility','hidden')
+        $('.lds-ring').css('visibility', 'hidden')
         alert('Please write some text');
-        throw 'Please write some text';
 
     };
 
@@ -18,40 +17,56 @@ $("#btn").click(function (event) {
     let pictureNumber = $('#numberInput').val();
 
     if (pictureNumber < 1) {
-        $('.lds-ring').css('visibility','hidden')
+        $('.lds-ring').css('visibility', 'hidden')
         alert('Please choose a quantity');
-        throw 'Please choose a quantity';
+        return;
+
 
     };
+
+
 
     let apiUrl = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=da9df3b628fe4099156c8892593f1d3c&text=${text}&sort=${sortSelector}&per_page=${pictureNumber}&format=json&nojsoncallback=1`
 
     $.getJSON(apiUrl, {
 
     }).done(function (data) {
-        let getImage = data.photos.photo;
-     
-        for (let i = 0; i < getImage.length; i++) {
-            let id = getImage[i].id;
-            let secret = getImage[i].secret;
-            let server = getImage[i].server;
-            let sizeInput = $('#size').val();
+        if (data.photos && data.photos.photo) {
 
-            const url = `https://live.staticflickr.com/${server}/${id}_${secret}_${sizeInput}.jpg`
+            let getImage = data.photos.photo;
+            if (getImage.length === 0) {
+                alert('No images found, please try again');
+            } else {
+          
+                for (let i = 0; i < getImage.length; i++) {
+                    let id = getImage[i].id;
+                    let secret = getImage[i].secret;
+                    let server = getImage[i].server;
+                    let sizeInput = $('#size').val();
 
-            const img = $('<img>');
-            img.attr('src', url);
-            $('#img-container').append(img);
-            $('img').css('margin','10px');
-            $('img').css('box-shadow', '10px 10px 5px #888');
-            $('.lds-ring').css('visibility','hidden');
+                    const url = `https://live.staticflickr.com/${server}/${id}_${secret}_${sizeInput}.jpg`
+
+                    const img = $('<img>');
+                    img.attr('src', url);
+                    $('#img-container').append(img);
+                    $('img').css('margin', '10px');
+                    $('img').css('box-shadow', '10px 10px 5px #888');
+                    $('.lds-ring').css('visibility', 'hidden');
+                }
+            } 
+        }else {
+           alert('No images found, please try again');
         }
-    }).fail(function (jqXHR, textStatus, erorrThrown) {
-        console.log(jqXHR, textStatus, erorrThrown);
-        $('.lds-ring').css('visibility','hidden');
+    }).fail(function (jqXHR, textStatus, errorThrown) {
+        console.log(jqXHR, textStatus, errorThrown);
+        $('.lds-ring').css('visibility', 'hidden');
+        alert('An error occurred while searching for images, please try again.');
     });
-   
+
 });
+
+
+
 
 
 
